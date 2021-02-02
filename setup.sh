ee2817321035edae910f067134cfb89454d763a6
orellanaluke <67909347+orellanaluke@users.noreply.github.com>
2021-02-02 14:49:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 cd /cloudacademy
 git clone https://github.com/cloudacademy/ca-hands-on-terragrunt-azure
 cp -r ./ca-hands-on-terragrunt-azure/$1/* /cloudacademy/lab
+chmod -R 777 /cloudacademy
 find "/cloudacademy" -name "*.tf" | xargs sed -i "s/REPLACEME/$2/g"
 find "/cloudacademy" -name "*.hcl" | xargs sed -i "s/REPLACEME/$2/g"
